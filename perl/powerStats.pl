e9f5239a90f67aa27bf41f27bcb3e2cc8f58465a
vasilis <vasileiosvlachos@gmail.com>
2015-08-10 08:38:11 +0100

    Just got the first numbers out

--- perl/powerStats.pl
+++ perl/powerStats.pl
@@ -13,10 +13,14 @@ $|=1;
 getPower();
 
 sub getPower {
-
+    my ($load, $watts);
     my $apc = new vPAN::APC;
     my $stat = $apc->apc();
-    print dump($stat);
-
+    print dump($stat)."\n";
+    $load =$stat->{'LOADPCT'};
+    $load =~ s/\s*[A-z].*//g;
+    $watts = $load*7/sqrt(2);
+    say "LOAD: $load";
+    say "WATTS: $watts";
 }
 
