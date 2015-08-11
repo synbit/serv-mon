f15d0ebb39d065341c78764bebe99d22d1907b93
vasilis <vasileiosvlachos@gmail.com>
2015-08-11 13:39:35 +0100

    powerStats.pl works; needs reviewing probably...

--- perl/powerStats.pl
+++ perl/powerStats.pl
@@ -7,6 +7,7 @@ use Config::Simple;
 use DBI;
 use feature qw(say);
 use vPAN::APC;
+use Sys::Hostname;
 $|=1;
 
 getPower();
@@ -24,12 +25,31 @@ sub getPower {
     $power->{'load%'} = $load;
     $power->{'watts'} = $watts;
 
+    print dump($power);
     saveToMySQL($power);
 }
 
 sub saveToMySQL {
 
     my ($power) = @_;
-    print dump($power);
+    my $config = new Config::Simple('Config/stats.conf');
+    my $dsn = $config->param('dsn');
+    my $uname = $config->param('uname');
+    my $passwd = $config->param('passwd');
+    my $ups_id = $config->param('ups_id');
+    my $units = $config->param('ups_attached_units');
 
+    my $dbo = DBI->connect("$dsn","$uname","$passwd") or die ("Couldn't connect to database: '$dsn'\n");
+
+    my $stm = <<'SQL';
+    INSERT INTO eulinx.ups_power_stat(ups_id,units_attached,load_percent,watts,last_check)
+	VALUES(?,?,?,?,NOW());
+SQL
+
+    my $stm_power = $dbo->prepare($stm);
+
+    $stm_power->execute($ups_id, $units, $power->{'load%'}, $power->{'watts'})
+	or die("Couldn't execute statement:\n$stm");
+
+    $dbo->disconnect() or die "Couldn't disconnect from MySQL (maybe there isn't an active connection).\n";
 }
