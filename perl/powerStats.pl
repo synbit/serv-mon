62fa9679781c63e3ed6d84b11a7501a02b9055cc
vasilis <vasileiosvlachos@gmail.com>
2015-08-11 13:04:47 +0100

    I'd rather have everything in a hash (I might push this into the APC module)

--- perl/powerStats.pl
+++ perl/powerStats.pl
@@ -3,7 +3,6 @@
 use strict;
 use warnings;
 use Data::Dump qw(dump);
-use Sys::Hostname;
 use Config::Simple;
 use DBI;
 use feature qw(say);
@@ -13,14 +12,24 @@ $|=1;
 getPower();
 
 sub getPower {
-    my ($load, $watts);
+    my ($load, $watts, $power);
+
     my $apc = new vPAN::APC;
     my $stat = $apc->apc();
-    print dump($stat)."\n";
+
     $load =$stat->{'LOADPCT'};
     $load =~ s/\s*[A-z].*//g;
     $watts = $load*7/sqrt(2);
-    say "LOAD: $load";
-    say "WATTS: $watts";
+
+    $power->{'load%'} = $load;
+    $power->{'watts'} = $watts;
+
+    saveToMySQL($power);
 }
 
+sub saveToMySQL {
+
+    my ($power) = @_;
+    print dump($power);
+
+}
