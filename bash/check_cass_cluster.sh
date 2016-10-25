ee68d65cbff3c57bb193e8664533089cef80af2f
vasilis <vasileiosvlachos@gmail.com>
2016-10-25 10:44:24 +0100

    Previously, if the node from which the check was executed was down, no problems would be reported

--- bash/check_cass_cluster.sh
+++ bash/check_cass_cluster.sh
@@ -7,18 +7,19 @@
 
 which cassandra || echo "UNKNOWN - Apache Cassandra is not installed on this server" && exit 3;
 
-NODETOOL=$(which nodetool);
-NODES_DOWN=$(${NODETOOL} --host localhost status | grep --count -E '^D[A-Z]');
+NODETOOL_OUTPUT=$(nodetool status 2> /dev/null);
+NODETOOL_EXIT_CODE=$?;
+NODES_DOWN=$(echo "${NODETOOL_OUTPUT}" | grep --count -E '^D[A-Z]');
 
-if [[ ${NODES_DOWN} -gt 0 ]]; then
-    output="CRITICAL - Nodes down: ${NODES_DOWN}";
-    return_code=2;
+if [[ ${NODETOOL_EXIT_CODE} -ne 0 ]]; then
+        output="UNKNOWN - Couldn't retrieve cluster information. Maybe nodetool was executed on a box where C* is down?";
+        return_code=3;
+elif [[ ${NODES_DOWN} -gt 0 ]]; then
+        output="CRITICAL - Nodes down: ${NODES_DOWN}";
+        return_code=2;
 elif [[ ${NODES_DOWN} -eq 0 ]]; then
-    output="OK - Nodes down: ${NODES_DOWN}";
-    return_code=0;
-else
-    output="UNKNOWN - Couldn't retrieve cluster information.";
-    return_code=3;
+        output="OK - Nodes down: ${NODES_DOWN}";
+        return_code=0;
 fi
 
 echo "${output}";
